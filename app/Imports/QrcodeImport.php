687f4acfa48d38155ebc8a2245f4feec21bbe5f0
Kan jtecna <kan_jtecna@hotmail.com>
2024-04-10 12:57:58 +0700

    dev

---
+++ app/Imports/QrcodeImport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Imports;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+
+class QrcodeImport implements ToCollection
+{
+    /**
+    * @param Collection $collection
+    */
+    public function collection(Collection $rows)
+    {
+        foreach ($rows as $row) 
+        {
+            if (empty($row[0])) {
+                continue;
+            }
+        }
+    }
+}
---
+++ resources/views/qrcode/index.blade.php
@@ -0,0 +1,63 @@
+@extends('frontend.layouts.master')
+
+@section('title')
+    {{ config('app.name') }} | {{ config('app.description') }}
+@endsection
+
+@section('main-content')
+    <main class="main">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 mt-5">
+
+                    @if (session('status'))
+                        <div class="alert alert-success">{{ session('status') }}</div>
+                    @endif
+
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Tạo mã QR code</h4>
+                        </div>
+                        <div class="card-body">
+
+                            <form action="{{ url('qrcode') }}" method="POST" enctype="multipart/form-data">
+                                @csrf
+                                <div class="input-group">
+                                    <input type="file" name="import_file" class="form-control" />
+                                    <button type="submit" class="btn btn-primary">Nhập dữ liệu</button>
+                                </div>
+
+                            </form>
+
+                            <hr>
+                            @if (@$collection)
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Mã</th>
+                                            <th>QR code</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($collection[0] as $item)
+                                            @if ($item[0])
+                                                <tr>
+                                                    <td>{{ $item[0] }}</td>
+                                                    <td style="text-align: center;">{!! QrCode::size(250)->generate((string) $item[0]) !!} </td>
+                                                </tr>
+                                            @endif
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+@endsection
+
+@section('scripts')
+    <script></script>
+@endsection
